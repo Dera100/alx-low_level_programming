9d7528d1988e841d7fc9351e405028f828e457c5
Dera100 <dnjubigbo@gmail.com>
2024-01-21 12:14:07 -0800

    kkk

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index area
+ * delete_nodeint_at_index - A  node in a linked lIST..
  *
  * @head: pointer to the first element in the list area
  *
