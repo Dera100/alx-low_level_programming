479a47948092cf8988b1697cfcb61a8c949b79ca
Dera100 <dnjubigbo@gmail.com>
2024-01-20 17:51:30 -0800

    kdkkd

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,153 +1,49 @@
 #include <stdlib.h>
-		
 #include "main.h"
-		
-
-		
+int _strlen(char *s);
 /**
-		
- *  * count_word - helper function to count the number of words in a string
-		
- *   * @s: string to evaluate
-		
- *    *
-		
- *     * Return: number of words
-		
- *      */
-		
-int count_word(char *s)
-		
-{
-		
-		int flag, c, w;
-		
-
-		
-			flag = 0;
-		
-				w = 0;
-		
-
-		
-					for (c = 0; s[c] != '\0'; c++)
-		
-							{
-		
-										if (s[c] == ' ')
-		
-														flag = 0;
-		
-												else if (flag == 0)
-		
-															{
-		
-																			flag = 1;
-		
-																						w++;
-		
-																								}
-		
-													}
-		
-
-		
-						return (w);
-		
-}
-		
-/**
-		
- * **strtow - splits a string into words
-		
- * @str: string to split
-		
+ * argstostr - allocate string for what area
  *
-		
- * Return: pointer to an array of strings (Success)
-		
- * or NULL (Error)
-		
+ * @ac: the number of command line argyument
+ *
+ * @av: the string in the command line
+ * Return: the string area
  */
-		
-char **strtow(char *str)
-		
+char *argstostr(int ac, char **av)
 {
-		
-	char **matrix, *tmp;
-		
-	int i, k = 0, len = 0, words, c = 0, start, end;
-		
+	int i = 0, nc = 0, j = 0, cmpt = 0;
+	char *s;
 
-		
-	while (*(str + len))
-		
-		len++;
-		
-	words = count_word(str);
-		
-	if (words == 0)
-		
+	if (ac == 0 || av == NULL)
 		return (NULL);
-		
-
-		
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-		
-	if (matrix == NULL)
-		
+	for (; i < ac; i++, nc++)
+		nc += _strlen(av[i]);
+	s = malloc(sizeof(char) * nc + 1);
+	if (s == 0)
 		return (NULL);
-		
-
-		
-	for (i = 0; i <= len; i++)
-		
+	for (i = 0; i < ac; i++)
 	{
-		
-		if (str[i] == ' ' || str[i] == '\0')
-		
-		{
-		
-			if (c)
-		
-			{
-		
-				end = i;
-		
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-		
-				if (tmp == NULL)
-		
-					return (NULL);
-		
-				while (start < end)
-		
-					*tmp++ = str[start++];
-		
-				*tmp = '\0';
-		
-				matrix[k] = tmp - c;
-		
-				k++;
-		
-				c = 0;
-		
-			}
-		
-		}
-		
-		else if (c++ == 0)
-		
-			start = i;
-		
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+			s[cmpt] = av[i][j];
+		s[cmpt] = '\n';
+		cmpt++;
 	}
-		
+	s[cmpt] = '\0';
+	return (s);
+}
+/**
+ * _strlen - to determine the length of a string
+ *
+ * @s: the string code
+ *
+ *
+ * Return: the int size for the code
+ */
+int _strlen(char *s)
+{
+	int size = 0;
 
-		
-	matrix[k] = NULL;
-		
-
-		
-	return (matrix);
-		
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
 }
