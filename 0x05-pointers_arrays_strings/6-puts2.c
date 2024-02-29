c4c9413c4705ca39077b1890e85c4e7792bdf929
Dera100 <njubigbodera@yahoo.com>
2024-02-29 03:59:36 -0800

    loiui

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string code.
+ *
+ * @str: character
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < string_length(str); i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+  * string_length - finds the length of a string code.
+  *
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
